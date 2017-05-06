1db1d8f0bdf957a1519e82f822bec8b11d913392
Nasib <Nasib>
2017-05-06 17:29:07 +0500

    adding contact us and unit detail page

---
+++ resources/views/contactus.blade.php
@@ -0,0 +1,76 @@
+@extends ('layout.master')
+
+
+<section class="map-container top-container">
+
+    <div class="mid-container clearfix">
+
+        <div class="col-xs-12"> <div id="map"></div></div>
+        
+
+    </div>
+
+</section>
+<%--    AIzaSyDozcm4BObnYJBHVQFZiIFgYgDcTQwADfI--%>
+
+
+
+    <section class="contact-us">
+        <div class="mid-container clearfix">
+            <div class="col-xs-12  col-sm-3 text-container">
+              
+                <div>
+                 <img src="images/logo.png" width="200" height="61"/>
+                </div>
+
+                 <h4 class="heading"> Address</h4>
+                <p class="para">Office 2602, Business Bay, Dubai</p>
+
+                  <h4 class="heading">Phone & Fax</h4>
+                <p class="para">Tel : +971 52 4474 455</p>
+
+                  <h4 class="heading">Email</h4>
+                <p class="para">info@appello.ae</p>
+          
+                  </div>
+
+
+            <div class="col-xs-12  col-sm-9 contact-us-form">
+
+                <h2 class="heading">
+                    Contact Us
+                </h2>
+                <p class="para">Want to know more about our services? Fill out the form below and a representative will contact you within 3 business days to discuss your enquiry. Alternatively, you can give us a ring Monday through Friday, 9:00 am to 6:00 pm on +92 21 34970226.</p>
+
+
+                <div class="col-xs-6 form-group ">
+                    <input type="text" class="form-control" placeholder="*First Name"/>
+                </div>
+                 <div class="col-xs-6 form-group ">
+                    <input type="text"  class="form-control"  placeholder="*First Name"/>
+                </div>
+
+                 <div class="col-xs-6 form-group ">
+                    <input type="text "  class="form-control"  placeholder="*First Name"/>
+                </div>
+
+                 <div class="col-xs-6 form-group ">
+                    <input type="text"  class="form-control" placeholder="*First Name"/>
+                </div>
+
+                 <div class="col-xs-6 form-group ">
+                    <input type="text"  class="form-control" placeholder="*First Name"/>
+                </div>
+
+                 <div class="col-xs-6 form-group ">
+                    <input type="text"  class="form-control" placeholder="*First Name"/>
+                </div>
+
+                 <div class="col-xs-12 form-group ">
+                  <textarea rows="5" title="message" placeholder="Message:"  class="form-control"></textarea>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
---
+++ resources/views/unitdetail.blade.php
@@ -0,0 +1,311 @@
+@extends ('layout.master')
+
+
+    <section class="property-detail-wraper">
+
+        <div class="mid-container">
+            <h1 class="heading">PROPERTY DETAILS - 2</h1>
+            <p class="para">Serving you since 1999. Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+
+        </div>
+
+    </section>
+
+
+    <section class="property-detail-slider">
+
+        <article class="mid-container">
+            <h2><strong>987 CANTEBURY DRIVE</strong></h2>
+            <p>45 Regent Street, London, UK</p>
+        </article>
+
+
+        <div class="owl-slider-container mid-container">
+            <div id="single-slider" class="owl-carousel single-slider owl-theme">
+                <div class="item">
+                    <img src="http://cdn.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/314870/1123332_Image.jpg&width=1140&group=1002&module=13&watermarkType=Default&position=" />
+                </div>
+                <div class="item">
+                    <img src="http://cdn.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/314870/1123332_Image.jpg&width=1140&group=1002&module=13&watermarkType=Default&position=" />
+                </div>
+
+            </div>
+
+            <div id="multislider" class="owl-carousel multislider owl-theme">
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+                <div class="item">
+                    <img src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&width=218&height=170&&group=1002&module=13&watermarkType=Default&position=~Lounge/Living" />
+                </div>
+
+
+
+
+            </div>
+        </div>
+
+
+        <div class="property-amienties mid-container padding15 clearfix">
+
+            <div class="col-xs-12  am-div col-sm-3"><span>4800 sq ft</span></div>
+             <div class="col-xs-12 am-div col-sm-3"><span>2 Office Rooms</span></div>
+             <div class="col-xs-12 am-div col-sm-3"><span>1 Bathroom</span></div>
+             <div class="col-xs-12 am-div col-sm-3"><span>TV Lounge</span></div>
+
+
+        </div>
+
+
+        <section class="para-unit-detail mid-container padding15">
+            <h2><strong>PROPERTY DESCRIPTION</strong></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et dui vestibulum, bibendum purus sit amet, vulputate mauris. Ut adipiscing gravida tincidunt. Duis euismod placerat rhoncus. Phasellus mollis imperdiet placerat. Sed ac turpis nisl. Mauris at ante mauris. Aliquam posuere fermentum lorem, a aliquam mauris rutrum a. Curabitur sit amet pretium lectus, nec consequat orci.</p>
+            <p>
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis et metus in libero sollicitudin venenatis eu sed enim. Nam felis lorem, suscipit ac nisl ac, iaculis dapibus tellus. Cras ante justo, aliquet quis placerat nec, molestie id turpis.
+            </p>
+        </section>
+
+
+
+
+
+        <div  class="Quick-sumary mid-container clearfix padding15">
+            <h2><strong>QUICK SUMMARY</strong></h2>
+
+            <div class="col-xs-12 col-sm-6 paddingL ">
+                <table class="table table-striped table-responsive">
+                    <tr>
+                        <td class="text-left"><strong>Property Id</strong></td>
+                        <td class="text-right">5456</td>
+                        
+                    </tr>
+
+                      <tr>
+                        <td class="text-left"><strong>Price</strong></td>
+                        <td class="text-right">$8,600 / month</td>
+                        
+                    </tr>
+
+
+                      <tr>
+                        <td class="text-left"><strong>Property Size</strong></td>
+                        <td class="text-right">5,500 ft2</td>
+                        
+                    </tr>
+
+
+                      <tr>
+                        <td class="text-left"><strong>Bedrooms</strong></td>
+                        <td class="text-right">5</td>
+                        
+                    </tr>
+
+
+                      <tr>
+                        <td class="text-left"><strong>Bathrooms</strong></td>
+                        <td class="text-right">3</td>
+                        
+                    </tr>
+
+
+                      <tr>
+                        <td class="text-left"><strong>Available From</strong></td>
+                        <td class="text-right">22-04-2017</td>
+                        
+                    </tr>
+                </table>
+            </div>
+
+
+               <div class="col-xs-12 col-sm-6 ">
+                <table class="table table-striped table-responsive">
+                    <tr>
+                        <td class="text-left"><strong>Property Id</strong></td>
+                        <td class="text-right">5456</td>
+                        
+                    </tr>
+
+                      <tr>
+                        <td class="text-left"><strong>Price</strong></td>
+                        <td class="text-right">$8,600 / month</td>
+                        
+                    </tr>
+
+
+                      <tr>
+                        <td class="text-left"><strong>Property Size</strong></td>
+                        <td class="text-right">5,500 ft2</td>
+                        
+                    </tr>
+
+
+                      <tr>
+                        <td class="text-left"><strong>Bedrooms</strong></td>
+                        <td class="text-right">5</td>
+                        
+                    </tr>
+
+
+                      <tr>
+                        <td class="text-left"><strong>Bathrooms</strong></td>
+                        <td class="text-right">3</td>
+                        
+                    </tr>
+
+
+                      <tr>
+                        <td class="text-left"><strong>Available From</strong></td>
+                        <td class="text-right">22-04-2017</td>
+                        
+                    </tr>
+                </table>
+            </div>
+
+        </div>
+
+
+
+        <div class="feature-property mid-container clearfix padding15">
+            <h2><strong>FEATURES</strong></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et dui vestibulum, bibendum purus sit amet, vulputate mauris. Ut adipiscing gravida tincidunt.</p>
+
+
+            <div class="col-xs-4 paddingL"><img class="img-responsive" src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&amp;width=360&amp;height=218&amp;&amp;group=1002&amp;module=13&amp;watermarkType=Default&amp;position=~Lounge/Living"></div>
+
+                   <div class="col-xs-4"><img class="img-responsive" src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&amp;width=360&amp;height=218&amp;&amp;group=1002&amp;module=13&amp;watermarkType=Default&amp;position=~Lounge/Living"></div>
+
+               <div class="col-xs-4"><img class="img-responsive" src="http://content.gomasterkey.com/images/watermark.aspx?imageurl=/uf/1002/unit/339886/1693643_Image.jpg&amp;width=360&amp;height=218&amp;&amp;group=1002&amp;module=13&amp;watermarkType=Default&amp;position=~Lounge/Living"></div>
+
+        </div>
+
+
+
+
+        <div class="agent-section clearfix mid-container padding15">
+            <h2><strong>CONTACT AGENT</strong></h2>
+
+
+            <div class="agent-img col-sm-4 col-xs-12 paddingL">
+                <img src="images/agent.png" /></div>
+            
+            <div class="agent-detail-txt col-sm-4 col-xs-12">
+                <h3>Bohdan Kononets</h3>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh tempor cum soluta nobis consectetuer adipiscing eleifend option congue nihil imperdiet doming…</p>
+                <table class="table">
+                    <tr>
+                        <td class="text-left"><strong>Phone:</strong></td>
+                        <td class="text-right">(+01) 34 56 7890</td>
+                    </tr>
+
+
+                    <tr>
+                        <td class="text-left"><strong>Mobile:</strong></td>
+                        <td class="text-right">(+033) 34 56 7890</td>
+                    </tr>
+
+
+
+                    <tr>
+                        <td class="text-left"><strong>Email Adress:</strong></td>
+                        <td class="text-right">bohdan@castle.com</td>
+                    </tr>
+
+
+
+                    <tr>
+                        <td class="text-left"><strong>Skype:</strong></td>
+                        <td class="text-right">bohdan.kononets</td>
+                    </tr>
+
+
+
+
+                </table>
+
+                <div class="social-icon-container">
+                        <span class="f"><a href=""><i class="fa fa-facebook" aria-hidden="true"></i>     </a>  </span>
+                        <span class="g"><a href=""><i class="fa fa-google-plus" aria-hidden="true"></i>  </a>      </span>
+                        <span class="t"><a href=""><i class="fa fa-twitter" aria-hidden="true"></i>      </a>      </span>
+                        <span class="v"><a href=""><i class="fa fa-vimeo" aria-hidden="true"></i>        </a>      </span>
+                    </div>
+            </div>
+
+
+            <div class="form col-sm-4 col-xs-12">
+                <div class="form-container">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="First Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Last Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email Address">
+                    </div>
+                    <div class="form-group">
+                        <input type="number" class="form-control" placeholder="Phone Number">
+                    </div>
+                    <div class="form-group">
+
+                        <select class="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+
+                        <button type="submit" class="btn btn-group-justified  btn-theme">Enqury</button>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+        </div>
+    </section>
